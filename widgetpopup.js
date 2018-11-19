68f53952e1b0906c94d8b5d74012a5e1fa869f1c
Emily Franz-Lien <hashtagappsteam@gmail.com>
2018-11-19 12:08:40 +0700

    website widget stuff

--- widgetpopup.js
+++ widgetpopup.js
@@ -1,9 +1,58 @@
-function prepareFrame() {
-	var ifrm = document.createElement('iframe');
-	ifrm.setAttribute('src', 'http://google.com/');
-	ifrm.style.width = '640px';
-	ifrm.style.height = '480px';
-	document.body.appendChild(ifrm);
-}
+(function() {
+	// Localize jQuery variable
+	var jQuery;
 
-prepareFrame();
+	/******** Load jQuery if not present *********/
+	if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.4.2') {
+		var script_tag = document.createElement('script');
+		script_tag.setAttribute('type', 'text/javascript');
+		script_tag.setAttribute(
+			'src',
+			'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'
+		);
+		if (script_tag.readyState) {
+			script_tag.onreadystatechange = function() {
+				// For old versions of IE
+				if (
+					this.readyState == 'complete' ||
+					this.readyState == 'loaded'
+				) {
+					scriptLoadHandler();
+				}
+			};
+		} else {
+			script_tag.onload = scriptLoadHandler;
+		}
+		// Try to find the head, otherwise default to the documentElement
+		(
+			document.getElementsByTagName('head')[0] || document.documentElement
+		).appendChild(script_tag);
+	} else {
+		// The jQuery version on the window is the one we want to use
+		jQuery = window.jQuery;
+		main();
+	}
+
+	/******** Called once jQuery has loaded ******/
+	function scriptLoadHandler() {
+		// Restore $ and window.jQuery to their previous values and store the
+		// new jQuery in our local jQuery variable
+		jQuery = window.jQuery.noConflict(true);
+		// Call our main function
+		main();
+	}
+
+	/******** Our main function ********/
+	function main() {
+		jQuery(document).ready(function($) {
+			/******* Load HTML *******/
+			var jsonp_url =
+				'http://al.smeuh.org/cgi-bin/webwidget_tutorial.py?callback=?';
+			$.getJSON(jsonp_url, function(data) {
+				$('#example-widget-container').html(
+					'This data comes from another server: ' + data.html
+				);
+			});
+		});
+	}
+})(); // We call our anonymous function immediately
